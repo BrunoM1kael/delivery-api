91640feaa3e406d8053a40d78e2760782ed82268
BrunoM1kael <brunomikael370@gmail.com>
2025-10-31 19:03:05 -0300

    Atualizando

---
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/service/ClienteService.java
@@ -0,0 +1,14 @@
+package com.delivery_api.Projeto.Delivery.API.service;
+
+import com.delivery_api.Projeto.Delivery.API.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class ClienteService {
+
+}
